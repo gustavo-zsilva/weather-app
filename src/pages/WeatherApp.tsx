59e397b2cf7afcf42d9f994c94bd1ab43be25f12
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2020-11-03 00:43:44 -0300

    Added useAsyncEffect && Finished base project
    useAsyncEffect for handling useEffect async functions with Typescript types infered.

--- src/pages/WeatherApp.tsx
+++ src/pages/WeatherApp.tsx
@@ -1,49 +1,54 @@
-import React, {useEffect, useState} from 'react';
-import axios, {AxiosPromise} from 'axios';
-import options from '../services/api';
-
-// import api from '../services/api';
+import React, {EffectCallback, useEffect, useState} from 'react';
+import { useAsyncEffect } from 'use-async-effect';
+import axios from 'axios';
+import api from '../services/api';
 
 import '../styles/pages/weather-app.css';
 
-interface Weather {
-    id: number;
-    main: string;
-    description: string;
-    icon: string;
-}
-
-interface ReturnedData {
-    weather: Array<Weather>;
+interface Location {
+    city: string;
+    country: string;
 }
 
 function WeatherApp() {
-    const [weatherData, setWeatherData] = useState([])
+    let time = new Date;
 
-    async function getWeatherData() {
-        const response = await axios.request(options);
-        console.log(response);
-        
-        return response.data.weather;
-    }
+    const [weather, setWeather] = useState<string>('clear')
+    const [temperature, setTemperature] = useState<number>(300)
+    const [location, setLocation] = useState<Location>({city: 'London', country: 'UK'})
+    const [timestamp, setTimestamp] = useState(`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`)
 
-    // useEffect(async () => {
-    //     const response: [] = await getWeatherData();
-    //     setWeatherData(response);
-    // }, [])
-
+    setInterval(() => {
+        time = new Date;
+        setTimestamp(`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`)
+    }, 1000)
     
-      
+    useAsyncEffect(async () => {
+        const response = await axios.request(api);
 
+        setTemperature(response.data.main.temp);
+        setLocation({city: response.data.name, country: response.data.sys.country});
+        setWeather(response.data.weather[0].description);
+    }, [])
+      
+    console.log(String(time.getMinutes()).length);
 
     return (
         <div className="container">
             <main>
-                <p className="current-weather-description">{}</p>
-                <p className="current-weather">19 c</p>
-                <p className="city">San Francisco</p>
-                <p className="current-time" onClick={getWeatherData}>Today, 4:05am</p>
-
+                <p className="current-weather-description">{ weather }</p>
+                <p className="current-weather">{ Math.round(temperature - 273.15) }°C</p>
+                <p className="city">{ location.city }, { location.country }</p>
+                <div className="current-time">
+                    <div>Today</div>
+                    ,
+                    <div className="hours-minutes">
+                        {timestamp}
+                    </div>
+                    <div className="year">
+                        {time.getFullYear()}
+                    </div>
+                </div>
             </main>
         </div>
     )
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,17 +1,19 @@
-import axios, {AxiosRequestConfig} from "axios";
+import {AxiosRequestConfig} from "axios";
 
-const options: AxiosRequestConfig = {
-    method: "GET",
-    url: "https://rapidapi.p.rapidapi.com/weather",
+const api: AxiosRequestConfig = {
+    method: 'GET',
+    url: 'https://rapidapi.p.rapidapi.com/weather',
     params: {
-        q: "Pelotas, Brasil",
-        units: '"metric" or "imperial"'
+        q: 'Pelotas,Brasil',
+        id: '2172797',
+        lang: 'null',
+        units: '"metric" or "imperial"',
+        mode: 'xml, html'
     },
     headers: {
-        "x-rapidapi-key": "072955421fmsh5cb0500004a41dep13f166jsn38f2f22d84e7",
-        "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+        'x-rapidapi-key': '072955421fmsh5cb0500004a41dep13f166jsn38f2f22d84e7',
+        'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com'
     }
 }
 
-
-export default options;
+export default api;
